959717a24dbf182fcbdb3fd34431c5539e760c33
Ezzeddine Jlassi <jlassiezzeddine@gmail.com>
2021-03-30 10:23:01 +0100

    use schema added

--- Backend/Models/Contact.js
+++ Backend/Models/Contact.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
-const contactSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   firstName: { type: String, require: true, unique: true },
   lastName: { type: String, require: true, unique: true },
   email: { type: String, require: true, unique: true },
   phone: { type: Number },
 });
+
+module.exports = mongoose.model('User', userSchema)
